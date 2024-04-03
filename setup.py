71a98e9abd810e33e8a10937e508d14164c6d2a5
Tanuj Sharma <tanujsharma3846@gmail.com>
2024-04-03 21:03:36 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Tanuj Sharma',
+    author_email= 'tanujsharma3846@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
